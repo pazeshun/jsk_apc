40c74dfca7ec2df866f259e9768939c320e8ae41
Shun Hasegawa <pazeshun3684@gmail.com>
2019-09-03 13:59:21 +0900

    Print only one sensor and reformat

--- jsk_arc2017_baxter/node_scripts/synchronized_proximity_printer.py
+++ jsk_arc2017_baxter/node_scripts/synchronized_proximity_printer.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
+# Usage:
+# rosrun jsk_arc2017_baxter synchronized_proximity_printer.py \
+#   ~input/tof:=/gripper_front/limb/left/tof_low_pass_filter/output \
+#   ~input/prox:=/gripper_front/limb/left/intensity_prox_calibrator/output \
+#   > test.log
+# In another terminal:
+# rosservice call /synchronized_proximity_printer/start_printing
+
 from __future__ import print_function
+
 import pprint
 
 import message_filters
-import numpy as np
 import rospy
 from sphand_driver_msgs.msg import IntensityProxCalibInfoArray
 from std_srvs.srv import Empty
@@ -16,6 +24,8 @@ class SynchronizedProximityPrinter(object):
 
     def __init__(self):
         self.print_num = rospy.get_param('~print_num', 5)
+        # Default target is right tip sensor
+        self.target_idx = rospy.get_param('~target_index', 3)
         self.current_num = 0
 
         self.start_printing_srv = rospy.Service(
@@ -49,12 +59,13 @@ class SynchronizedProximityPrinter(object):
     def printer(self, tof, prox):
         if self.current_num == 0:
             rospy.loginfo('Printing starts')
-        print('====================')
-        print('Data {}:'.format(self.current_num))
-        print('ToF:')
-        pprint.pprint(tof)
-        print('Proximity:')
-        pprint.pprint(prox)
+        print('Target sensor index: {}'.format(self.target_idx))
+        print('========================================')
+        print('# [Data {}]'.format(self.current_num))
+        print('## [ToF]')
+        pprint.pprint(tof.array[self.target_idx])
+        print('## [Proximity]')
+        pprint.pprint(prox.data[self.target_idx])
         self.current_num = self.current_num + 1
         if self.current_num >= self.print_num:
             rospy.loginfo('Printing ends')
